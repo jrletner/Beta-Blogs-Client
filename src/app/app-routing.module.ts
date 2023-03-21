ded54cd67ad471c137c9a7e8c38f0543d581ad45
Joe Letner <jrletner@gmail.com>
2023-03-20 21:30:00 -0500

    added signup component and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
