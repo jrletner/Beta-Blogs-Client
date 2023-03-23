e6f634e6b03493855e30f29ac45f8c48e673d4b6
Joe Letner <jrletner@gmail.com>
2023-03-22 23:24:49 -0500

    added signup and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    NavComponent
-  ],
+  declarations: [AppComponent, SignupComponent, NavComponent, LoginComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
