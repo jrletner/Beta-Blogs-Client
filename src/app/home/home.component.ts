a036cc1d61f2211ea5fa1c891ca522727f9c9ea3
Joe Letner <jrletner@gmail.com>
2023-04-01 23:42:33 -0500

    added data to blogs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../shared/services/blog.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  suggestedBlogs: any = [];
+  categories: any = [];
 
-  constructor() { }
+  constructor(private blogService: BlogService) {}
 
   ngOnInit(): void {
+    this.blogService.fetchBlogs().subscribe((res: any) => {
+      console.log(res);
+      if (res.success) {
+        this.suggestedBlogs = res.payload.suggested;
+        this.categories = res.payload.categories;
+      }
+    });
   }
-
 }
---
+++ src/app/shared/services/blog.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BlogService {
+  constructor(private http: HttpClient) {}
+
+  fetchBlogs() {
+    return this.http.get('http://localhost:3000/api/v1/blogs/home');
+  }
+}
